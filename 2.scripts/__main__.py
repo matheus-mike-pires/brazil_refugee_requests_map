2460deaa6ff1ebc151c79b926b5dc3fb210e432d
Matheus de O. Pires <matheusolv.pires@gmail.com>
2026-09-17 09:18:16 -0300

    Update __main__.py

--- 2.scripts/__main__.py
+++ 2.scripts/__main__.py
@@ -1,33 +1,24 @@
 from  __statistics__ import *
 from __savedata__ import *
 import pandas as pd
-from verifym import *
-running_program = True
-import os
 
-update_data = load_raw_data()
-
-df1 = update_data.drop(['PAIS_DE_NASCIMENTO', 'EST_CIV', 'MUN_RECEBIMENTO'], axis=1)
-dataframe = df1
+running_program = []
 
 print('***********************')
 print('This is a python program made to explore the data on refugees  in Brasil')
 print('The analisys mainly relies on the python library Pandas and on the statistical analysis of the data gathered. ')
 print('for more information, acess the repository: ')
-print('!!! Follow the instructions on READ.ME to download the necessary files and properly run this program !!!')
+print('!!! Follow the instructions on READ.ME to download the necessary files and properly run this program !!!\n')
 print('***********************')
 
-
-def main(dataframe):
-    print()
+def main(dataframe, running_program):
     valid_options = ['1', '2', '3', '4', '5', '6']
-    print('1. Verify the full database of refuge requests')
+    print('\n1. Verify the full database of refuge requests')
     print('2. Filter the database')
     print('3. Verify the statistcs')
     print('4. Request contact')
     print('5. How to use')
-    print('6. Exit')
-    print()
+    print('6. Exit \n')
     choice = input('select an option: ')
     while choice not in valid_options:
         choice = input('please, select a valid option: ')
@@ -45,12 +36,12 @@ def main(dataframe):
         print('click on the README.md file')
         print('Follow the instructions displayed \n')       
     if choice == '6':
-        print('Thank you for using this program')
+        running_program.append(1)
+        print('Thank you for using this program \n')
         running_program = False
     
     
 
 if __name__ == '__main__':
-    while running_program == True:
-        #test_me_now(dataframe)
-        main(dataframe)
+    while len(running_program) == 0:
+        main(dataframe, running_program)
